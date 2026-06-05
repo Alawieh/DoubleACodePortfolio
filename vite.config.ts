b4ab666ae3f7b04f8efca7dfc67c336cce37f366
Hussein <123alawieh@gmail.com>
2026-06-05 15:04:17 +0300

    Configure Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  plugins: [tanstackStart(), react(), tailwindcss(), tsConfigPaths()],
+  plugins: [tanstackStart(), nitro(), react(), tailwindcss(), tsConfigPaths()],
 });
